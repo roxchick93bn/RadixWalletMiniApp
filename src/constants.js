ac1c51f8138dca0c773c139c7e574290f7e33d9b
roxchick93bn <shinydev618@gmail.com>
2024-09-05 08:48:27 +0200

    fix: dappdefinitionaddress

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 
 export const dAppDefinitionAddress =
-  "account_rdx1283mafsdzvsg5ymht7wem794vg8r4k52kapjlffmxn70cc7n0aqwlv";
+  "account_rdx128qkjn3elftzjc8zq8cgz26nk6g9n9vrlrt20ascycw5py782eucza";
