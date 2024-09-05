c4476326b1a312c58a6557fc2f03398236b69ef8
roxchick93bn <shinydev618@gmail.com>
2024-09-05 07:53:10 +0200

    fix: mainnet address

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,3 @@
-export const componentAddress =
-  "component_tdx_2_1cz44jlxyv0wtu2cj7vrul0eh8jpcfv3ce6ptsnat5guwrdlhfpyydn";
 
-// You can create a dApp definition in the dev console at https://stokenet-console.radixdlt.com/dapp-metadata
-// then use that account for your dAppDefinitionAddress
 export const dAppDefinitionAddress =
   "account_rdx1283mafsdzvsg5ymht7wem794vg8r4k52kapjlffmxn70cc7n0aqwlv";
