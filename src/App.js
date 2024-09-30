1fb60ca4abc1877124eb22573a780b240d93708c
roxchick93bn <shinydev618@gmail.com>
2024-09-30 17:17:13 -0400

    add telegram

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import ConnectButton from "./components/ConnectButton";
 import InputResource from "./components/InputResource";
 
 function App() {
+  const data = window.Telegram.WebApp.initData;
+  const platform = window.Telegram.WebApp.platform;
+
+  console.log(platform);
   return (
     <>
       <div className="p-5">
         <ConnectButton />
       </div>
+      {platform === "tdesktop" ? (
+        <div>This is Desktop</div>
+      ) : platform === "tmobile" ? (
+        <div>This is Mobile</div>
+      ) : (
+        <div>Undefined</div>
+      )}
     </>
   );
 }
